94dd1cc5b7a1dc0d57dba17bf1c4ba0f678cd8ce
Hrutvik <hrutvikjagtap51@gmail.com>
2019-07-26 22:39:57 +0530

    Clone dependencies manually * As Dependency_cloner.py dosent seem to work

--- clone.sh
+++ clone.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/hsj51/android_device_lenovo_A6020 -b 9.x device/lenovo/A6020
+git clone https://github.com/hsj51/android_kernel_lenovo_msm8916 -b lineage-16.0-rebase kernel/lenovo/msm8916
+git clone https://github.com/hsj51/android_vendor_lenovo_A6020 -b lineage-16.0 vendor/lenovo/A6020
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
+git clone https://github.com/lineageos/android_external_stlport external/stlport
+git clone https://github.com/lineageos/android_external_bson external/bson
 
 # write commands if you want to clone some repos
