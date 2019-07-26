1e710e2dac88dd1df8cf8ca46492eb8c6d1c527c
Hrutvik <hrutvikjagtap51@gmail.com>
2019-07-26 18:52:48 +0530

    Update

--- config.sh
+++ config.sh
@@ -9,11 +9,11 @@ KBUILD_BUILD_HOST="HrutvikJagtap"
 export oem=lenovo
 export device=A6020
 
-ROM="AEX
+ROM="AEX"
 manifest_url="https://github.com/AospExtended/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="9.x
+branch="9.x"
 
-release_repo="jazz511/A6020_releases
+release_repo="jazz511/A6020_releases"
 
 timezone="Asia/Kolkata"
